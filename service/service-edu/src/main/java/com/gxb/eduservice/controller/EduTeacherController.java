34788ebc02b9bf62ffe4784ba09539d53822d555
Geligamesh <535758155@qq.com>
2020-05-01 22:37:47 +0800

    讲师模块

--- service/service-edu/src/main/java/com/gxb/eduservice/controller/EduTeacherController.java
+++ service/service-edu/src/main/java/com/gxb/eduservice/controller/EduTeacherController.java
@@ -25,6 +25,7 @@ import java.util.List;
 @Api(tags = "讲师管理")
 @RestController
 @RequestMapping("/eduservice/teacher")
+@CrossOrigin
 public class EduTeacherController {
 
     //访问地址： http://localhost:8001/eduservice/teacher/findAll
@@ -80,42 +81,44 @@ public class EduTeacherController {
         return R.ok().data("total",total).data("rows",records);
     }
 
-    //4 条件查询带分页的方法
     @PostMapping("pageTeacherCondition/{current}/{limit}")
     public R pageTeacherCondition(@PathVariable long current,@PathVariable long limit,
-                                  @RequestBody(required = false) TeacherQuery teacherQuery) {
+                                  @RequestBody(required = false)  TeacherQuery teacherQuery) {
         //创建page对象
-        Page<EduTeacher> pageTeacher = new Page<>(current,limit);
+        Page<EduTeacher> pageTeacher = new Page<>(current, limit);
 
         //构建条件
         QueryWrapper<EduTeacher> wrapper = new QueryWrapper<>();
-       // 多条件组合查询
+        // 多条件组合查询
         // mybatis学过 动态sql
         String name = teacherQuery.getName();
         Integer level = teacherQuery.getLevel();
         String begin = teacherQuery.getBegin();
         String end = teacherQuery.getEnd();
         //判断条件值是否为空，如果不为空拼接条件
-        if(!StringUtils.isEmpty(name)) {
+        if (!StringUtils.isEmpty(name)) {
             //构建条件
-            wrapper.like("name",name);
+            wrapper.like("name", name);
         }
-        if(!StringUtils.isEmpty(level)) {
-            wrapper.eq("level",level);
+        if (!StringUtils.isEmpty(level)) {
+            wrapper.eq("level", level);
         }
-        if(!StringUtils.isEmpty(begin)) {
-            wrapper.ge("gmt_create",begin);
+        if (!StringUtils.isEmpty(begin)) {
+            wrapper.ge("gmt_create", begin);
         }
-        if(!StringUtils.isEmpty(end)) {
-            wrapper.le("gmt_create",end);
+        if (!StringUtils.isEmpty(end)) {
+            wrapper.le("gmt_create", end);
         }
 
+        //排序
+        wrapper.orderByDesc("gmt_create");
+
         //调用方法实现条件查询分页
-        teacherService.page(pageTeacher,wrapper);
+        teacherService.page(pageTeacher, wrapper);
 
         long total = pageTeacher.getTotal();//总记录数
         List<EduTeacher> records = pageTeacher.getRecords(); //数据list集合
-        return R.ok().data("total",total).data("rows",records);
+        return R.ok().data("total", total).data("rows", records);
     }
 
     //添加讲师接口的方法
